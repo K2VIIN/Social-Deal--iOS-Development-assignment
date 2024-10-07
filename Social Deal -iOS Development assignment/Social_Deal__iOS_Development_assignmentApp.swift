a33a642c26e4a85a2ebfa3942f017f6d78262f36
Kevin Schuren <k.schuren@student.avans.nl>
2024-10-07 20:19:27 +0200

    Initial Commit

---
+++ Social Deal -iOS Development assignment/Social_Deal__iOS_Development_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_Deal__iOS_Development_assignmentApp.swift
+//  Social Deal -iOS Development assignment
+//
+//  Created by Kevin Schuren on 07/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Social_Deal__iOS_Development_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
